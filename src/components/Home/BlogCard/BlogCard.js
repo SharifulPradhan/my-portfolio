b511564788f905fec912b90c172539e52611d5b9
Shariful Pradhan Hridoy <sharifulpradhanhridoy@Gmail.com>
2021-05-18 13:59:06 +0600

    blog section update

--- src/components/Home/BlogCard/BlogCard.js
+++ src/components/Home/BlogCard/BlogCard.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
 
-const BlogCard = () => {
+const BlogCard = ({blogs}) => {
+  const {cover, title, description, link} = blogs;
   return (
     <Card style={{ width: '18rem' }} className='border rounded shadow mb-5 ml-3 mr-3'>
-      <Card.Img variant="top" src="https://miro.medium.com/max/700/1*X3P5a3VqgPYqk_bbkmJMrw.jpeg" />
+      <Card.Img variant="top" src={cover}/>
       <Card.Body>
-        <Card.Title>Top 10 Things I have Learned by Exploring Javascript Today.</Card.Title>
-        <Card.Text>
-        I make my day somewhere valuable by learning more about JavaScript. So I will give a brief description of what I have learned so far...
-        </Card.Text>
-        <a href="https://medium.com/10-things-i-have-learned-by-exploring-java-script/top-10-things-i-have-learned-by-exploring-javascript-today-75a0a572d694"><button className="button-style">Full Blog</button></a>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>{description}</Card.Text>
+        <a href={link} target="_blank" rel="noreferrer"><button className="button-style">Full Blog</button></a>
       </Card.Body>
     </Card>
   );
